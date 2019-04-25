6deb2d09d3a9b9ee109b1b2b6893ef0a28f30d6f
hcgustavo <ghcaixeta@gmail.com>
2019-04-25 08:14:05 -0300

    [FIX] fetch questions url

--- src/actions/questionsActions.js
+++ src/actions/questionsActions.js
@@ -1,7 +1,7 @@
 import { FETCH_QUESTIONS } from './types';
 
 export const fetchQuestions = () => dispatch => {
-    fetch('https://conjugapp-backend.herokuapp.com/api/questions', {mode: 'no-cors'})
+    fetch('http://localhost:8080/api/questions')
     .then(res => res.json())
     .then(questions => 
         dispatch({
