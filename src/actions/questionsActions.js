4bc8e2c327582b5c03ea292cf417d3ba3117a0b3
hcgustavo <ghcaixeta@gmail.com>
2019-04-25 08:15:09 -0300

    url

--- src/actions/questionsActions.js
+++ src/actions/questionsActions.js
@@ -1,7 +1,7 @@
 import { FETCH_QUESTIONS } from './types';
 
 export const fetchQuestions = () => dispatch => {
-    fetch('http://localhost:8080/api/questions')
+    fetch('https://conjugapp-backend.herokuapp.com/api/questions')
     .then(res => res.json())
     .then(questions => 
         dispatch({
